3377e84ec3fe04b88c5c0dda511926d34f801f08
m4rcos-dev <dev.marcospereira@gmail.com>
2023-03-08 22:10:34 -0300

    :sparkles: feat: create loginRoute and add loginRout in the app

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,9 +1,9 @@
 const { User } = require('../models');
 
-const login = (email, password) => {
+const login = async (email, password) => {
   if (!email || !password) return { status: 400, message: 'Some required fields are missing' };
-  const emailVerify = User.findOne({ where: { email } });
-  const passwordVerify = User.findOne({ where: { password } });
+  const emailVerify = await User.findOne({ where: { email } });
+  const passwordVerify = await User.findOne({ where: { password } });
   if (!emailVerify || !passwordVerify) return { status: 400, message: 'Invalid fields' };
   const token = 'testetoken';
   return { status: 200, result: token };
