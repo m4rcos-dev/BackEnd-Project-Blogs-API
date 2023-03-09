5965aecbab507c8353c789350cb837445da1b4e9
m4rcos-dev <dev.marcospereira@gmail.com>
2023-03-09 00:21:59 -0300

    :recycle: refactor: loginServices and LoginController for add generateToken

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -2,9 +2,9 @@ const loginService = require('../services/loginService');
 
 const login = async (req, res) => {
   const { email, password } = req.body;
-  const { status, message, result } = await loginService.login(email, password);
+  const { status, message, token } = await loginService.login(email, password);
   if (message) return res.status(status).json({ message });
-  return res.status(status).json(result);
+  return res.status(status).json({ token });
 };
 
 module.exports = { login };
--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,12 +1,15 @@
 const { User } = require('../models');
+const { generateToken } = require('../auth/authFunctions');
 
 const login = async (email, password) => {
   if (!email || !password) return { status: 400, message: 'Some required fields are missing' };
-  const emailVerify = await User.findOne({ where: { email } });
-  const passwordVerify = await User.findOne({ where: { password } });
-  if (!emailVerify || !passwordVerify) return { status: 400, message: 'Invalid fields' };
-  const token = 'testetoken';
-  return { status: 200, result: token };
+  const userByEmail = await User.findOne({ where: { email } });
+  if (!userByEmail
+    || userByEmail.password !== password) return { status: 400, message: 'Invalid fields' };
+  const { password: _, ...userWithoutPassword } = userByEmail.dataValues;
+  console.log(userWithoutPassword);
+  const token = generateToken(userWithoutPassword);
+  return { status: 200, token };
 };
 
 module.exports = { login };
