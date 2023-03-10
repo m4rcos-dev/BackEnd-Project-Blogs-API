7e142a694a80a5dfac9ee9692f889d0da455a8f5
m4rcos-dev <dev.marcospereira@gmail.com>
2023-03-10 16:41:29 -0300

    :white_check_mark: test: Requirement 6

--- src/services/userService.js
+++ src/services/userService.js
@@ -17,7 +17,7 @@ const getAllUsers = async () => {
 };
 
 const getById = async (id) => {
-  const user = await User.findOne({ where: { id } });
+  const user = await User.findOne({ where: { id }, attributes: { exclude: ['password'] } });
   if (!user) return { status: 404, message: 'User does not exist' };
   return { status: 200, user };
 };
