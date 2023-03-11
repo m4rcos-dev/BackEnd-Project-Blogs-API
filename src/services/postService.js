ad84c073337aeba5e58d38e54ac33e1da6561878
m4rcos-dev <dev.marcospereira@gmail.com>
2023-03-10 22:41:02 -0300

    :sparkles: feat: create postService with createPost function

--- src/services/postService.js
+++ src/services/postService.js
@@ -1,7 +1,17 @@
-// const { BlogPost } = require('../models');
+const { BlogPost } = require('../models');
 
-// const createPost = async () => {
+const createPost = async (body) => {
+  const { title, content } = body;
+  const { id } = body.currentUser;
+  const currentPost = {
+    title,
+    content,
+    userId: id,
+    updated: new Date(),
+    published: new Date(),
+  };
+  const postCreated = await BlogPost.create(currentPost);
+  return postCreated;
+};
 
-// };
-
-// module.exports = { createPost };
+module.exports = { createPost };
