d89ba0ce6c6ba48ab7de73be530f8d89ac0b55f1
m4rcos-dev <dev.marcospereira@gmail.com>
2023-03-10 23:36:11 -0300

    :sparkles: feat: create middleware validatePost

---
+++ src/middleware/validatePost.js
@@ -0,0 +1,19 @@
+const { Category } = require('../models');
+
+const validatePost = async (req, res, next) => {
+  const { title, content, categoryIds } = req.body;
+  if (!title || !content || !categoryIds) {
+    return res.status(400).json({ message: 'Some required fields are missing' });
+  }
+  const verifyCategory = categoryIds.map(async (category) => {
+    const currentCategory = await Category.findOne({ where: { id: category } });
+    return !currentCategory;
+  });
+  const verifyAllCategories = verifyCategory.every((valid) => !valid);
+  if (!verifyAllCategories) {
+    return res.status(400).json({ message: 'one or more "categoryIds" not found' });
+  }
+  next();
+};
+
+module.exports = validatePost;
