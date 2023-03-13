ae3412fec51ad2d71a3b445623596366aa86f07b
m4rcos-dev <dev.marcospereira@gmail.com>
2023-03-13 15:51:47 -0300

    :recycle: refactor: PostCategory model to fix primaryKey and associations alias

--- src/models/PostCategory.js
+++ src/models/PostCategory.js
@@ -1,30 +1,30 @@
-/**
- *
- * @param {import('sequelize')} sequelize
- * @param {import('sequelize')} DataTypes
- */
-
 module.exports = (sequelize, DataTypes) => {
-const PostCategory = sequelize.define('PostCategory',{
-  postId: DataTypes.INTEGER,
-  categoryId: DataTypes.INTEGER,
-}, {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+  }, {
     tableName: 'posts_categories',
     timestamps: false,
     underscored: true,
   });
-  PostCategory.associate = ({BlogPost, Category}) => {
+  PostCategory.associate = ({ BlogPost, Category }) => {
     BlogPost.belongsToMany(Category, {
       foreignKey: 'postId',
       otherKey: 'categoryId',
       through: 'PostCategory',
-      as: 'post',
+      as: 'category',
     });
     Category.belongsToMany(BlogPost, {
       foreignKey: 'categoryId',
       otherKey: 'postId',
       through: 'PostCategory',
-      as: 'category',
+      as: 'post',
     })
   }
   return PostCategory;
