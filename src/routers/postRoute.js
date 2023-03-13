41af446b6d8f37e35b1bf28e636c423158e878be
m4rcos-dev <dev.marcospereira@gmail.com>
2023-03-13 16:48:21 -0300

    :sparkles: feat: create put in the post endpoint for update post

--- src/routers/postRoute.js
+++ src/routers/postRoute.js
@@ -8,5 +8,6 @@ const postRoute = express.Router();
 postRoute.post('/', validateToken, validatePost, postController.createPost);
 postRoute.get('/', validateToken, postController.getAllPosts);
 postRoute.get('/:id', validateToken, postController.getPostById);
+postRoute.put('/id', validateToken, postController.updatePost);
 
 module.exports = postRoute;
