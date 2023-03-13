8277b776158251829a3b96bc4d1d5ccd8aa24039
m4rcos-dev <dev.marcospereira@gmail.com>
2023-03-13 19:48:25 -0300

    :recycle: refactor: validateUpdatePost middleware to fix correct return

--- src/middleware/validateUpdatePost.js
+++ src/middleware/validateUpdatePost.js
@@ -3,14 +3,17 @@ const { getPostById } = require('../services/postService');
 const validadeUpdatePost = async (req, res, next) => {
   const { title, content } = req.body;
   const { id } = req.params;
-  const idUserPost = getPostById(id).user.id;
-  const idCurrentUser = req.body.currentUser;
+  const idCurrentUser = req.body.currentUser.id;
 
   if (!title || !content) {
     return res.status(400)
       .json({ message: 'Some required fields are missing' });
   }
 
+  const { status, message, currentPost } = await getPostById(id);
+  if (message) return res.status(status).json({ message });
+
+  const idUserPost = currentPost.dataValues.userId;
   if (idUserPost !== idCurrentUser) return res.status(401).json({ message: 'Unauthorized user' });
 
   next();
