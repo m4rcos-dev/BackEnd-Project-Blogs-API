28dedfc0fef214041cbb69f4262741533ae22d95
m4rcos-dev <dev.marcospereira@gmail.com>
2023-03-14 11:27:56 -0300

    :sparkles: feat: create swagger cofing to generate json file

---
+++ src/swagger/swagger.js
@@ -0,0 +1,19 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'Blogs API',
+    description: 'Documentation - Blogs API',
+  },
+  host: 'localhost:3000',
+  schemes: ['http'],
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['../routers/index.js'];
+
+/* NOTE: if you use the express Router, you must pass in the
+   'endpointsFiles' only the root file where the route starts,
+   such as index.js, app.js, routes.js, ... */
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
