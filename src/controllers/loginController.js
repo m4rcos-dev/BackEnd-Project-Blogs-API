2b0af0c13449d77de8c795a59d9cfa225180aeff
m4rcos-dev <dev.marcospereira@gmail.com>
2023-03-14 14:11:26 -0300

    :recycle: refactor: comments loginController to fix correct return

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -12,19 +12,12 @@ const login = async (req, res) => {
 } */
 
   /* #swagger.responses[200] = {
-              description: 'If the login was successful, the result returned should be as shown below, with a status',
-              schema: {
-                  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7ImlkIjo1LCJkaXNwbGF5TmFtZSI6InVzdWFyaW8gZGUgdGVzdGUiLCJlbWFpbCI6InRlc3RlQGVtYWlsLmNvbSIsImltYWdlIjoibnVsbCJ9LCJpYXQiOjE2MjAyNDQxODcsImV4cCI6MTYyMDY3NjE4N30.Roc4byj6mYakYqd9LTCozU1hd9k_Vw5IWKGL4hcCVG8',
-              }
+              description: 'Token',
       } */
 
   /* #swagger.responses[400] = {
-        description: `1- If the request does not have all the fields duly completed (there cannot be blank fields), the returned result should be as shown below
-        </br>2 - If the request receives a wrong/non-existent email and password pair, the returned result should be as shown below`,
-        schema: [{
-            1: {message: 'Some required fields are missing'},
-            2: {message: 'Invalid fields'}
-        }]
+        description: `Some required fields are missing
+        <hr>Invalid fields`,
 } */
 
   const { email, password } = req.body;
