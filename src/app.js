b228488bdd0c3f3006188d126a8a8dc276c09700
m4rcos-dev <dev.marcospereira@gmail.com>
2023-03-14 11:09:43 -0300

    :sparkles: feat: create swagger route in the app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const swaggerDocument = require('./swagger/swagger.json');
 const { loginRoute, userRoute, categoryRoute, postRoute } = require('./routers');
 
 // ...
@@ -20,6 +20,7 @@ app.use('/login', loginRoute);
 app.use('/user', userRoute);
 app.use('/categories', categoryRoute);
 app.use('/post', postRoute);
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
