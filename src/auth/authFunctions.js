aaef9ac6f66cf272cedf37c75f9cccd0c52722d8
m4rcos-dev <dev.marcospereira@gmail.com>
2023-03-15 00:23:00 -0300

    :sparkles: reactor: create available variables to deploy railway

--- src/auth/authFunctions.js
+++ src/auth/authFunctions.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const secret = process.env.JWT_SECRET || '26467ada8f3cf8b04a65ef10818a9f4c117e9b19';
+const secret = process.env.JWT_SECRET;
 
 const JWT_CONFIG = {
   algorithm: 'HS256',
